d205cf25edf5e20c1eed9081092eb0d27f2eb577
Juan Francisco Foncubierta <juanfra@democratest.com>
2023-02-01 15:08:21 +0100

    Finished the functionalities, i just have to correct things

--- RobotGameTests/TestsPlaceRobot.cs
+++ RobotGameTests/TestsPlaceRobot.cs
@@ -2,4 +2,61 @@
 
 internal class TestsPlaceRobot
 {
+    [Test]
+    public void TestEmptyString()
+    {
+        RobotGame.GameBoard board = new(1, 1, 5, 5);
+        board.placeRobot("");
+        Assert.True(board.ObjectsOnTheBoard.Count() == 0);
+    }
+
+    [Test]
+    public void TestNoDataForRobot()
+    {
+        RobotGame.GameBoard board = new(1, 1, 5, 5);
+        board.placeRobot("place_robot");
+        Assert.True(board.ObjectsOnTheBoard.Count() == 0);
+    }
+
+    [Test]
+    public void TestOutOfBoardSuperior()
+    {
+        RobotGame.GameBoard board = new(1, 1, 5, 5);
+        board.placeRobot("place_robot 6,6,NORTH");
+        Assert.True(board.ObjectsOnTheBoard.Count() == 0);
+    }
+
+    [Test]
+    public void TestOutOfBoardInferior()
+    {
+        RobotGame.GameBoard board = new(1, 1, 5, 5);
+        board.placeRobot("place_robot 0,0,SOUTH");
+        Assert.True(board.ObjectsOnTheBoard.Count() == 0);
+    }
+
+    [Test]
+    public void TestWithWrongFacing()
+    {
+        RobotGame.GameBoard board = new(1, 1, 5, 5);
+        board.placeRobot("place_robot 1,1,A");
+        Assert.True(board.ObjectsOnTheBoard.Count() == 0);
+    }
+
+    [Test]
+    public void TestCorrectCommand()
+    {
+        RobotGame.GameBoard board = new(1, 1, 5, 5);
+        board.placeRobot("place_robot 1,1,NORTH");
+        Robot robot = (Robot)board.ObjectsOnTheBoard.First();
+        Assert.True(robot.positionX == 1 && robot.positionY == 1 && robot.faceDirection == Directions.directions.NORTH);
+    }
+
+    [Test]
+    public void Test1()
+    {
+        RobotGame.GameBoard board = new(1, 1, 5, 5);
+        board.placeRobot("place_robot 5,3,EAST");
+        Robot robot = (Robot)board.ObjectsOnTheBoard.First();
+        Assert.True(robot.positionX == 5 && robot.positionY == 3 && robot.faceDirection == Directions.directions.EAST);
+    }
 }
--- RobotGameTests/TestsPlaceWall.cs
+++ RobotGameTests/TestsPlaceWall.cs
@@ -2,4 +2,52 @@
 
 internal class TestsPlaceWall
 {
+    [Test]
+    public void TestEmptyString()
+    {
+        RobotGame.GameBoard board = new(1, 1, 5, 5);
+        board.placeWall("");
+        Assert.True(board.ObjectsOnTheBoard.Count() == 0);
+    }
+
+    [Test]
+    public void TestNoDataForWall()
+    {
+        RobotGame.GameBoard board = new(1, 1, 5, 5);
+        board.placeWall("place wall");
+        Assert.True(board.ObjectsOnTheBoard.Count() == 0);
+    }
+
+    [Test]
+    public void TestOutOfBoardInferior()
+    {
+        RobotGame.GameBoard board = new(1, 1, 5, 5);
+        board.placeWall("place wall 0,0");
+        Assert.True(board.ObjectsOnTheBoard.Count() == 0);
+    }
+
+    [Test]
+    public void TestOutOfBoardSuperior()
+    {
+        RobotGame.GameBoard board = new(1, 1, 5, 5);
+        board.placeWall("place wall 6,3");
+        Assert.True(board.ObjectsOnTheBoard.Count() == 0);
+    }
+
+    [Test]
+    public void TestCreateCorrectWall()
+    {
+        RobotGame.GameBoard board = new(1, 1, 5, 5);
+        board.placeWall("place_wall 1,1");
+        Assert.True(board.ObjectsOnTheBoard.Where(x => x.boardObjectType == BoardObjectType.Wall).Any() && board.ObjectsOnTheBoard.First().positionX == 1 && board.ObjectsOnTheBoard.First().positionY == 1);
+    }
+
+    [Test]
+    public void TestTryPutAWallOverARobot()
+    {
+        RobotGame.GameBoard board = new(1, 1, 5, 5);
+        board.placeRobot("place_robot 1,1,NORTH");
+        board.placeWall("place_wall 1,1");
+        Assert.True(board.ObjectsOnTheBoard.Count() == 1 && board.ObjectsOnTheBoard.First().boardObjectType == BoardObjectType.Robot);
+    }
 }
